7a41cb8db20327456da36f6e12d027437367f029
forfeen <f.fin07@gmail.com>
2021-05-19 16:01:45 +0700

    add provider

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import MoodDetailScreen from './src/screens/MoodDetailScreen';
 import MoodListScreen from './src/screens/MoodListScreen';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
+import { Provider as AuthProvider } from './src/context/AuthContext';
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -25,4 +26,11 @@ const switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+export default () => {
+  return(
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
