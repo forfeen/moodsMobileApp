bf83ca860aa9b0e4b47d8350ffc93b84ac45f937
forfeen <f.fin07@gmail.com>
2021-05-19 16:51:39 +0700

    finish sign up and navigate to index page

--- src/api/moods.js
+++ src/api/moods.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://7a43e56bab8b.ngrok.io'
+    baseURL: 'http://48d4f7eec667.ngrok.io'
 });
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,25 +1,32 @@
 import createDataContext from './createDataContext';
 import MoodsApi from '../api/moods';
+import { AsyncStorage } from 'react-native';
+import {navigate} from '../navigationRef';
+
 const authReducer = (state, action) => {
     switch (action.type){
         case 'add_error':
             return {...state, errorMessage: action.payload }
+        case 'signup':
+            return { errorMessage: '', token: action.payload}
         default:
             return state;
     }
 };
 
-const signup = dispatch => {
-    return async ({email, password}) => {
+const signup = dispatch => async ({email, password}) => {
         try{
             const response = await MoodsApi.post('/signup', { email, password});
-            console.log(response.data);
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({ type: 'signup', payload: response.data.token});
+            navigate('moodList')
+
         } catch (err) {
             dispatch({ type: 'add_error', payload: 'Something went wrong with sign up'})
         }
     };
 
-};
+
 
 // const signin = (dispatch) => {
 //     return ({email, password}) => {
@@ -38,5 +45,5 @@ const signup = dispatch => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signup},
-    { isSignedIn: false, errorMessage: '' }
+    { token: null, errorMessage: '' }
 );
