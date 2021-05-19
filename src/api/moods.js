0ea775683c51e5b8665dba33886f0d3829025343
forfeen <f.fin07@gmail.com>
2021-05-19 16:02:09 +0700

    add signup with api

---
+++ src/api/moods.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://7a43e56bab8b.ngrok.io'
+});
---
+++ src/context/AuthContext.js
@@ -0,0 +1,40 @@
+import createDataContext from './createDataContext';
+import MoodsApi from '../api/moods';
+const authReducer = (state, action) => {
+    switch (action.type){
+        default:
+            return state;
+    }
+};
+
+const signup = dispatch => {
+    return async ({email, password}) => {
+        try{
+            const response = await MoodsApi.post('/signup', { email, password});
+            console.log(response.data);
+        } catch (err) {
+            console.log(err.response.data);
+        }
+    };
+
+};
+
+// const signin = (dispatch) => {
+//     return ({email, password}) => {
+
+//     };
+
+// };
+
+// const signout = dispatch => {
+//     return () => {
+
+//     };
+
+// };
+
+export const { Provider, Context } = createDataContext(
+    authReducer,
+    {  signup},
+    { isSignedIn: false }
+);
